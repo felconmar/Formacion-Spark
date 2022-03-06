e4ba992aa0817cfb0c22ee1996c05a456342d038
Félix Conde Marrón <felix.conde@bosonit.com>
2022-03-06 20:34:29 +0100

    Ejercicios en Python y Scala del tema 2 de Learning Spark 2

---
+++ Python/Py/ejercicios_tema_2_spark20.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""Del ejercicio de M&M aplicar: 
+
+
+1.   Otras operaciones de agregación como el Max con otro tipo de 
+ordenamiento (descendiente).
+2.   Hacer un ejercicio como el “where” de CA que aparece en el libro pero 
+indicando más opciones de estados (p.e. NV, TX, CA, CO).
+3. Hacer un ejercicio donde se calculen en una misma operación el Max, 
+Min, Avg, Count. Revisar el API (documentación) donde encontrarán 
+este ejemplo:
+ds.agg(max($"age"), avg($"salary"))
+ds.groupBy().agg(max($"age"), avg($"salary"))
+NOTA: $ es un alias de col()
+4. Hacer también ejercicios en SQL creando tmpView
+"""
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import max, desc, asc, avg, count
+import os
+
+mnm_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'mnm_dataset.csv'))
+
+
+#Create SparkSession
+spark = (SparkSession
+  .builder
+  .appName("PythonMnMCount")
+  .getOrCreate())
+#Read dataset
+mnm_df = (spark.read.format("csv")
+  .option("header", "true")
+  .option("inferSchema", "true")
+  .load(mnm_path))
+
+
+
+def mnm_op():
+  
+#Select columns State, Color and Count, grouped by State and Color, aggregate Count with average, alias "Avg", order by "Avg"
+  op_mnm_df = (mnm_df
+    .select("State", "Color", "Count")
+    .groupBy("State", "Color")
+    .agg(avg("Count").alias("Avg"))
+    .orderBy("Avg", ascending=True))
+#Show 60 rows without any truncate and print the total of rows
+  op_mnm_df.show(n=60, truncate=False)
+  print("Total Rows = %d" % (op_mnm_df.count()))
+#Same as before but only from California
+  ca_op_mnm_df = (mnm_df
+    .select("State", "Color", "Count")
+    .where(mnm_df.State == "CA")
+    .groupBy("State", "Color")
+    .agg(avg("Count").alias("Avg"))
+    .orderBy(desc("Avg")))
+#Show only 10 rows without truncate
+  ca_op_mnm_df.show(n=10, truncate=False)
+
+mnm_op()
+
+
+def mnm_multiple_states():
+#Select columns State, Color and Count where state in [CA, WA, NV, TX], grouped by State and Color, aggregate Count with count, alias "Total", order by "Total"
+  multi_sats_op_mnm_df = (mnm_df
+    .where((mnm_df.State == "CA") | (mnm_df.State == "WA") | (mnm_df.State == "NV") | (mnm_df.State == "TX"))
+    .groupBy("Color", "State")
+    .agg(count("Count").alias("Total"))
+    .orderBy(desc("Total"))
+    .select("State", "Color", "Total"))
+#Show 60 rows without truncate
+  multi_sats_op_mnm_df.show(n=60, truncate=False)
+
+mnm_multiple_states()
+
+
+def mnm_multiple_agg():
+  #Select columns State, Color, Total, Max, Avg, grouped by State and Color, aggregate Count with (count alias "Total", max alias "Max", avg alias "Avg"), order by "State"
+  multi_agg_op_mnm_df = (mnm_df
+    .groupBy("State", "Color")
+    .agg(count("Count").alias("Total"), max("Count").alias("Max"),avg("Count").alias("Avg"))
+    .orderBy(desc("State"))
+    .select("State", "Color", "Total", "Max", "Avg"))
+#Show 60 without truncate
+  multi_agg_op_mnm_df.show(n=60, truncate=False)
+
+mnm_multiple_agg()
+
+
+def mnm_pysql():
+  mnm_df.createOrReplaceTempView("mnm_view") 
+  #Select columns State, Color and Count where State in [CA, TX, WA], grouped by State and Color, aggregate Count with average, order by "State"
+  sql_mnm_view = spark.sql("select State, Color, avg(Count) from mnm_view \
+        where State = 'CA' or State = 'TX' or State = 'WA' group by State, Color order by State;") 
+  #Show 60 without truncate
+  sql_mnm_view.show(n=60, truncate=False)
+
+mnm_pysql()
+
+
+
+# Stop the SparkSession
+spark.stop()
---
+++ Scala/Ejs_tema2_spark/src/main/scala/org/example/Quijote.scala
@@ -0,0 +1,43 @@
+package org.example
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+
+import java.util.concurrent.atomic.AtomicInteger
+
+
+object Quijote {
+  def main(args : Array[String]) {
+    val spark:SparkSession = SparkSession.builder().master("local[1]")
+      .appName("quijote")
+      .getOrCreate()
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val quijote_data = spark.sparkContext
+      .textFile("src/data/mm_ataset.csv")
+    val nlineas = quijote_data.count()
+    println("Número de líneas: " + nlineas)
+    val take = quijote_data.take(5)
+    println("Take: ")
+    take.foreach(unit=>println(unit))
+    val first = quijote_data.first()
+    println("First: " + first)
+    val auto_id  = new AtomicInteger(1)
+    val quijote_map = quijote_data.map(linea=>Row(auto_id.getAndIncrement(), linea))
+    println("Map:")
+    quijote_map.take(5).foreach(unit=>println(unit))
+    val schema = StructType(List(
+      StructField("ID", IntegerType, true),
+      StructField("linea", StringType, true)
+    ))
+    val quijote_df = spark.sqlContext.createDataFrame(quijote_map, schema)
+    println("Head DF: " + quijote_df.head(5))
+    println("First DF: " + quijote_df.first())
+    println("Show without truncate: ")
+    quijote_df.show(10)
+    println("Show with truncate: ")
+    quijote_df.show(10, truncate = 5)
+
+  }
+}
